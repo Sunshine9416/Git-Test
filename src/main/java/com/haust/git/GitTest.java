10cd5b547478b441c87f09dc77948cc88894097e
Sunshine <Sunshine@126.com>
2022-11-16 09:22:57 +0800

    Second Commit

--- src/main/java/com/haust/git/GitTest.java
+++ src/main/java/com/haust/git/GitTest.java
@@ -8,5 +8,6 @@ package com.haust.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git!");
+        System.out.println("hello,Git2!");
     }
 }
