3bce3b44ba4e3af22a5db89b3b3375f2d10b5116
Sunshine <Sunshine@126.com>
2022-11-16 09:20:50 +0800

    First Commit

---
+++ src/main/java/com/haust/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.haust.git;
+
+/**
+ * Date:2022/11/16
+ * Author£ºSunshine
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git!");
+    }
+}
